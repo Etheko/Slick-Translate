330451ad5630ddcf6d3f2e4324973bd818f57f75
Etheko <i.ruizba.2021@alumnos.urjc.es>
2023-10-12 17:25:45 +0200

    Commit inicial
    Integración de la API de LibreTranslate en Android Studio.
    
    Co-Authored-By: AlbitaVM <116177640+AlbitaVM@users.noreply.github.com>
    Co-Authored-By: Cristian A. Vlad <96200703+Cristian1A@users.noreply.github.com>
    Co-Authored-By: aadrisoriiano <116881512+aadrisoriiano@users.noreply.github.com>
    Co-Authored-By: Pedro Torrecilla <117302423+p-torrecilla@users.noreply.github.com>

---
+++ app/src/main/java/NewTranslator.java
@@ -0,0 +1,28 @@
+import net.suuft.libretranslate.*;
+
+import java.util.ArrayList;
+
+
+public class NewTranslator {
+    public static void main(String[] args) {
+        // TEST
+        String[] testTexts = {
+                "Hola mundo",
+                "You cannot untie a knot without knowing how it is tied.",
+                "「真の知恵は、自らの無知を認識することにある」。"
+        };
+        SpecificTranslator myTranslator = new SpecificTranslator();
+        System.out.println("1st translation: " + myTranslator.translate(Language.SPANISH, Language.ENGLISH, testTexts[0]));
+        System.out.println("2nd translation: " + myTranslator.translate(Language.NONE, Language.SPANISH, testTexts[1]));
+        System.out.println("3rd translation: " + myTranslator.translate(Language.JAPANESE, Language.ENGLISH, testTexts[2]));
+
+        ArrayList<TranslatorData> history = myTranslator.getHistory();
+        for (TranslatorData tData: history){
+            System.out.println("----------");
+            System.out.println("Text translated from " + tData.getFromLanguage() + " to " + tData.getToLanguage());
+            System.out.println(">>>" + tData.getOriginalText());
+            System.out.println(tData.getTranslatedText());
+        }
+        System.out.println("----------");
+    }
+}
