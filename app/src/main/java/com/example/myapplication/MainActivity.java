d13bda600edde4ea263f73daba4d7e45ccd25b1e
aadrisoriiano <aadrisoriiano@gmail.com>
2023-10-16 20:55:57 +0200

    SPINNERS

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,9 +5,15 @@ import android.content.ClipboardManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
 
     EditText multi1;
     EditText multi2;
+    Spinner spinner_origin;
+    String idioma_origin;
+    String idioma_destination;
+    Spinner spinner_destination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,53 @@ public class MainActivity extends AppCompatActivity {
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
+        Spinner spinnerorigin = (Spinner) findViewById(R.id.spinner_origin_lang);
+        Spinner spinnerdestination = (Spinner) findViewById(R.id.spinner_destination_lang);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.languages_array_origin, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.languages_array_destination, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerorigin.setAdapter(adapter);
+        spinnerdestination.setAdapter(adapter2);
+
+        spinner_origin = findViewById(R.id.spinner_origin_lang);
+        spinner_destination = findViewById(R.id.spinner_destination_lang);
+
+        // Configura el listener para el Spinner
+        spinner_origin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String seleccion_origin = parentView.getItemAtPosition(position).toString();
+                idioma_origin=seleccion_origin;
+                // "seleccion" contiene el texto del elemento seleccionado
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Manejar la ausencia de selección si es necesario
+            }
+        });
+
+        // Configura el listener para el Spinner
+        spinner_destination.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String seleccion_destination = parentView.getItemAtPosition(position).toString();
+                idioma_destination=seleccion_destination;
+                // "seleccion" contiene el texto del elemento seleccionado
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Manejar la ausencia de selección si es necesario
+            }
+        });
+
+
     }
 
     public void buttonPressed(View view) {
@@ -40,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             hideSoftKeyboard(this);
             Traductor translator = new Traductor(); //creo una clase traductor donde se haga la operacion de traducir para no hacerla aqui
-            String trText = translator.traducir(text);
+            String trText = translator.traducir(this.idioma_origin,this.idioma_destination,text);
             multi2.setText(trText);
         }
     }
@@ -56,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
             clipboard.setText(text2);
         }
     }
+
 
     public static void hideSoftKeyboard(Activity activity) {
         InputMethodManager inputMethodManager =(InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
--- app/src/main/java/com/example/myapplication/Traductor.java
+++ app/src/main/java/com/example/myapplication/Traductor.java
@@ -4,11 +4,16 @@ import net.suuft.libretranslate.Language;
 
 public class Traductor {
 
+
     SpecificTranslator translator = new SpecificTranslator();
 
     public Traductor() {}
 
-    public String traducir(String text) {
-        return translator.translate(Language.SPANISH, Language.ENGLISH, text);
+    public String traducir(String idioma_origin,String idioma_destination,String text) {
+        if (idioma_origin.equals("AUTO")){
+            idioma_origin = "NONE";
+        }
+        text = text.toLowerCase(); //para poder traducir si se escriben mayusculas
+        return translator.translate(Language.valueOf(idioma_origin), Language.valueOf(idioma_destination), text);
     }
 }
