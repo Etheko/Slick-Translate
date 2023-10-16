6d4c8eca8be9c4c89ab70f21a634307e193710d2
etentor <117302367+etentor@users.noreply.github.com>
2023-10-17 00:45:31 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,69 +1,51 @@
 package com.example.myapplication;
 
-import android.app.Activity;
-import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.WindowCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import java.util.Objects;
+import com.example.myapplication.fragments.SettingsFragment;
+import com.example.myapplication.fragments.TraductorFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText multi1;
-    EditText multi2;
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            Fragment selectedFragment = TraductorFragment.newInstance();
+            if (item.getItemId() == R.id.traductor) {
+                selectedFragment = TraductorFragment.newInstance();
+            } else if (item.getItemId() == R.id.historial) {
+                // historial fragment
+            } else if (item.getItemId() == R.id.ajustes) {
+                selectedFragment = SettingsFragment.newInstance();
+            }
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.content, selectedFragment);
+            transaction.commit();
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(R.layout.activity_main);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, TraductorFragment.newInstance());
+        transaction.commit();
     }
-
-    public void buttonPressed(View view) {
-        multi1 = findViewById(R.id.multiText1);
-        multi2 = findViewById(R.id.multiText2);
-        String text = multi1.getText().toString();
-
-        if (text.isEmpty()) {
-            Toast.makeText(this, "No hay texto para traducir", Toast.LENGTH_SHORT).show();
-        } else {
-            hideSoftKeyboard(this);
-            Traductor translator = new Traductor(); //creo una clase traductor donde se haga la operacion de traducir para no hacerla aqui
-            String trText = translator.traducir(text);
-            multi2.setText(trText);
-        }
-    }
-
-    public void copyButtonPressed(View view) {
-        multi2 = findViewById(R.id.multiText2);
-        String text2 = multi2.getText().toString();
-
-        if (text2.isEmpty()) {
-            Toast.makeText(this, "No hay texto para copiar", Toast.LENGTH_SHORT).show();
-        } else {
-            ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-            clipboard.setText(text2);
-        }
-    }
-
-    public static void hideSoftKeyboard(Activity activity) {
-        InputMethodManager inputMethodManager =(InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        if(inputMethodManager.isAcceptingText()){
-            inputMethodManager.hideSoftInputFromWindow(
-                    Objects.requireNonNull(activity.getCurrentFocus()).getWindowToken(),
-                    0
-            );
-        }
-    }
-
 }
